c8e47cb6b1e4d3a2d8499eafaa4e0d213637957b
ivansusiyanto <ivansusiyanto@gmail.com>
2021-08-14 15:50:39 +0700

    Create App

---
+++ connect-to-ec2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -i ~/personal-aws-thinkpad.pem ubuntu@18.138.212.152
---
+++ copy-to-ec2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp -r -i ~/personal-aws-thinkpad.pem ./program/* ubuntu@18.138.212.152:/home/ubuntu/app
